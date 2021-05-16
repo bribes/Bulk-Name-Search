a6401f44381f3de095a4bc69990efc7c5bf47bb3
Faav <52789876+withdrew@users.noreply.github.com>
2021-05-16 15:27:28 -0400

    Update checker.js

--- checker.js
+++ checker.js
@@ -54,53 +54,10 @@ var dropping = "The name you entered is dropping on "; //The Dropping Message
 
 input.value = username; //Sets the input value to the username
 if (username !== "") { //Checks if the username isn"t blank
-  fetch(API).then(response => response.json()).then((main) => {
-    if (main.error === true) { //Checks if there is a error
-      fetch(`https://api.gapple.pw/blocked/${username}`).then(response => response.json()).then((gapple) => {
-        var table = document.getElementById("myTable");
-        table.innerHTML = "<tr><td>" + errorMessage(username) + "</td></tr>";
-        if (errorMessage(username) == "No minecraft account currently has that username!") {
-          if (gapple.status == "blocked") {
-            document.getElementById("myTable").innerHTML = `<td>${blocked}</td>`; //Makes the error message
-          } else {
-            if (gapple.status == "soon") {
-              document.getElementById("myTable").innerHTML = `<td>${dropping}${formatDrop(gapple.drop_time)}.</td>`; //Makes the error message
-            }
-          }
-        }
-      });
-    } else {
-      window.username = main.data.player.username;
-      window.name_history = main.data.player.meta.name_history.reverse();
-      var icon = `https://api.ashcon.app/mojang/v2/avatar/${window.username}`; // The Favicon
-      var title = `${window.username} | Name History`; // The Title
-      buildTable(window.name_history); //Makes the Name History
-      document.title = title; //Adds the Title
-      document.getElementById("icon").href = icon; //Adds the Favicon
+  fetch('https://post-profiles.mcapi.workers.dev/', {
+    method: 'POST', body: JSON.stringify("Seeped, hi, marc".replaceAll(" ", "").split(",")),
+    headers: {
+      'Content-Type': 'application/json'
     }
-    //Name History Section
-    function buildTable(data) {
-      var table = document.getElementById("myTable");
-      if (data.length === 1) {
-        var row3 = `<tr class="bold">
-	  <td>${data.length}. <a href="?username=${data[0].name}">${data[0].name}</a><\/td>`;
-        table.innerHTML += row3;
-      } else {
-        var row = `<tr class="bold">
-	  <td>${data.length}. <a href="?username=${data[0].name}">${data[0].name}</a><\/td><td class="right">${formatTime(data[0].changedToAt)}<\/td>
-	                     <\/tr>`;
-        table.innerHTML += row;
-        for (var i = 1; i < data.length - 1; i++) {
-          var row1 = `<tr>
-	  <td>${(data.length - i)}. <a href="?username=${data[i].name}">${data[i].name}<\/a><\/td><td class="right">${formatTime(data[i].changedToAt)}<\/td>
-	                     <\/tr>`;
-          table.innerHTML += row1;
-        }
-        var row2 = `<tr>
-	  <td>${(data.length - i)}. <a href="?username=${data[i].name}">${data[i].name}</a><\/td>
-	                     <\/tr>`;
-        table.innerHTML += row2;
-      }
+  }).then(response => response.json()).then((main) => {console.log(main[(main.length - 1)].name)});
     }
-  });
-}
